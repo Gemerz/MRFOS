333ce597ea6bc0a686d7a9692889e57844619438
gemer cheung <gemercheung@gmail.com>
2013-07-06 15:25:52 +0800

    del

--- data/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array (
-  'DB_HOST' => '127.0.0.1',
-  'DB_NAME' => 'GSOA',
-  'DB_USER' => 'root',
-  'DB_PWD' => '',
-  'DB_PORT' => '3306',
-  'DB_PREFIX' => 'GS_',
-);
--- data/config/url.php
+++
@@ -1,6 +0,0 @@
-<?php 
-return array (
-  'URL_MODEL' => 0,
-  'URL_ROUTER_ON' => true,
-
-);
